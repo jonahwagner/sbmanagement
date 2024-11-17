c8f0cb7ff5b55c709d23f96028515f7f6206f362
jonahwagner <143041801+jonahwagner@users.noreply.github.com>
2024-11-17 11:07:27 -0500

    Update authconfig.js

--- authconfig.js
+++ authconfig.js
@@ -1,21 +1,15 @@
-// Configuration for MSAL.js
 const msalConfig = {
   auth: {
-    clientId: "26e0f56c-0f41-4f2e-836d-bc317bbbcb6a", // Replace with your Azure AD B2C App Registration's client ID
-    authority: "https://spiritbc.b2clogin.com/spiritbc.onmicrosoft.com/B2C_1_sbconnect", // Replace with your user flow's authority
-    redirectUri: "https://management.spiritbound.gg", // Replace with the redirect URI
-    postLogoutRedirectUri: "https://management.spiritbound.gg", // URL to redirect to after logout
+    clientId: "26e0f56c-0f41-4f2e-836d-bc317bbbcb6a", // From App Registration in Azure
+    authority: "https://spiritbc.b2clogin.com/spiritbc.onmicrosoft.com/B2C_1_sbconnect", // Replace with your tenant's authority
+    redirectUri: "https://management.spiritbound.gg/" // Make sure this matches your Azure setup
   },
   cache: {
-    cacheLocation: "sessionStorage", // This can be "localStorage" if you want longer persistence
-    storeAuthStateInCookie: false, // Recommended for modern browsers
-  },
+    cacheLocation: "localStorage", // For storing auth state
+    storeAuthStateInCookie: true
+  }
 };
 
 const loginRequest = {
-  scopes: ["openid", "profile", "email"], // Scopes to request
-};
-
-const tokenRequest = {
-  scopes: ["https://spiritbc.onmicrosoft.com/api/user.read"], // Example API scope for accessing protected resources
+  scopes: ["openid", "offline_access"]
 };
