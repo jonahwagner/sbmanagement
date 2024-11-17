a05fe0fbb175046d4d8d60aede8b26b4fbeaef86
jonahwagner <143041801+jonahwagner@users.noreply.github.com>
2024-11-17 11:15:24 -0500

    Update authconfig.js

--- authconfig.js
+++ authconfig.js
@@ -1,17 +1,15 @@
 const msalConfig = {
-  auth: {
-    clientId: "26e0f56c-0f41-4f2e-836d-bc317bbbcb6a", // Replace with your App Registration Client ID
-    authority: "https://spiritbc.b2clogin.com/spiritbc.onmicrosoft.com/B2C_1_sbconnect", // Your Azure AD B2C Authority URL
-    knownAuthorities: ["spiritbc.b2clogin.com"], // Ensures trust in your B2C tenant
-    redirectUri: "https://management.spiritbound.gg/" // Redirect URL after login
-  },
-  cache: {
-    cacheLocation: "localStorage", // Store tokens in localStorage
-    storeAuthStateInCookie: true // Recommended for older browsers
-  }
+    auth: {
+        clientId: "26e0f56c-0f41-4f2e-836d-bc317bbbcb6a", // Replace with your Azure AD B2C client ID
+        authority: "https://spiritbc.b2clogin.com/spiritbc.onmicrosoft.com/B2C_1_sbconnect", // Replace with your authority
+        redirectUri: "https://management.spiritbound.gg", // Your redirect URI
+    },
+    cache: {
+        cacheLocation: "localStorage", // Configure where your cache will be stored
+        storeAuthStateInCookie: true, // Set to "true" for IE11 or Edge
+    },
 };
 
-// Define request for authentication
 const loginRequest = {
-  scopes: ["openid", "offline_access"] // Required permissions for sign-in
+    scopes: ["openid", "offline_access"],
 };
