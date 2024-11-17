8f583b9f18c891072a0346c178a738ed7a1b1a8d
jonahwagner <143041801+jonahwagner@users.noreply.github.com>
2024-11-17 11:12:45 -0500

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,11 +1,10 @@
+// Ensure msalConfig is imported before initializing msalInstance
 const msalInstance = new msal.PublicClientApplication(msalConfig);
 
 function signIn() {
   msalInstance.loginPopup(loginRequest)
     .then((loginResponse) => {
       console.log("Login successful:", loginResponse);
-
-      // Redirect to home.html after login
       window.location.href = "home.html";
     })
     .catch((error) => {
@@ -14,14 +13,8 @@ function signIn() {
     });
 }
 
-// Check for a valid session or tokens in cache
-function checkAccount() {
-  const accounts = msalInstance.getAllAccounts();
-  if (accounts && accounts.length > 0) {
-    console.log("User is already logged in:", accounts[0]);
-    window.location.href = "home.html"; // Redirect to home.html if logged in
+window.onload = () => {
+  if (msalInstance.getAllAccounts().length > 0) {
+    window.location.href = "home.html";
   }
-}
-
-// Automatically check account on page load
-window.onload = checkAccount;
+};
