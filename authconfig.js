6302f73800df6ee04ebd5c925d929e452a7091b8
jonahwagner <143041801+jonahwagner@users.noreply.github.com>
2024-11-17 10:36:41 -0500

    Update authconfig.js

--- authconfig.js
+++ authconfig.js
@@ -1,18 +1,14 @@
 const msalConfig = {
-    auth: {
-      clientId: "26e0f56c-0f41-4f2e-836d-bc317bbbcb6a", // Replace with your Application (client) ID
-      authority: "https://spiritbc.b2clogin.com/spiritbc.onmicrosoft.com/B2C_1A_SignUpSignIn", // Replace with your B2C tenant and user flow
-      redirectUri: "https://management.spiritbound.gg/auth-callback", // Replace with your redirect URI
-    },
-    cache: {
-      cacheLocation: "localStorage",
-      storeAuthStateInCookie: true,
-    }
-  };
-  
-  const loginRequest = {
-    scopes: ["openid", "profile", "email"]
-  };
-  
-  const msalInstance = new msal.PublicClientApplication(msalConfig);
-  
+  auth: {
+    clientId: "26e0f56c-0f41-4f2e-836d-bc317bbbcb6a", // Replace with your Azure AD B2C application client ID
+    authority: "https://spiritbc.b2clogin.com/spiritbc.onmicrosoft.com/B2C_1_sbconnect", // Your B2C tenant and user flow
+    knownAuthorities: ["spiritbc.b2clogin.com"], // B2C tenant domain
+    redirectUri: "https://management.spiritbound.gg", // Your website's URL
+  },
+};
+
+const loginRequest = {
+  scopes: ["openid", "profile", "email"], // Scopes for the login request
+};
+
+const msalInstance = new msal.PublicClientApplication(msalConfig);
